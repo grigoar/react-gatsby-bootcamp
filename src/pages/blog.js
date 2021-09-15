7137d569c5f275e8d464f270c739a6b94c02e947
grigoar <grigore.nath.j@gmail.com>
2021-09-15 06:51:46 +0300

    rendering posts from CMS Contentful instead of local

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -5,26 +5,43 @@ import Layout from "../components/layout"
 import * as blogStyles from "./blog.module.scss"
 
 const BlogPage = () => {
-  const data = useStaticQuery(
+  // const data = useStaticQuery(
+  //   graphql`
+  //     query {
+  //       allMarkdownRemark {
+  //         edges {
+  //           node {
+  //             frontmatter {
+  //               title
+  //               date
+  //             }
+  //             fields {
+  //               slug
+  //             }
+  //           }
+  //         }
+  //       }
+  //     }
+  //   `
+  // )
+  const dataContentful = useStaticQuery(
     graphql`
       query {
-        allMarkdownRemark {
+        allContentfulBlogPost(sort: { fields: publishedDate, order: DESC }) {
           edges {
             node {
-              frontmatter {
-                title
-                date
-              }
-              fields {
-                slug
-              }
+              title
+              slug
+              publishedDate(formatString: "MMMM Do, YYYYY")
             }
           }
         }
       }
     `
   )
-  const blogs = data.allMarkdownRemark.edges
+  console.log(dataContentful)
+  // const blogs = data.allMarkdownRemark.edges
+  const blogs = dataContentful.allContentfulBlogPost.edges
   return (
     <Layout>
       <h1>My blog</h1>
@@ -32,9 +49,9 @@ const BlogPage = () => {
         {blogs.map((blog, index) => {
           return (
             <li className={blogStyles.post} key={index}>
-              <Link to={`/blog/${blog.node.fields.slug}`}>
-                <h2>{blog.node.frontmatter.title}</h2>
-                <p>{blog.node.frontmatter.date}</p>
+              <Link to={`/blog/${blog.node.slug}`}>
+                <h2>{blog.node.title}</h2>
+                <p>{blog.node.publishedDate}</p>
               </Link>
             </li>
           )
@@ -42,6 +59,23 @@ const BlogPage = () => {
       </ol>
     </Layout>
   )
+  // return (
+  //   <Layout>
+  //     <h1>My blog</h1>
+  //     <ol className={blogStyles.posts}>
+  //       {blogs.map((blog, index) => {
+  //         return (
+  //           <li className={blogStyles.post} key={index}>
+  //             <Link to={`/blog/${blog.node.fields.slug}`}>
+  //               <h2>{blog.node.frontmatter.title}</h2>
+  //               <p>{blog.node.frontmatter.date}</p>
+  //             </Link>
+  //           </li>
+  //         )
+  //       })}
+  //     </ol>
+  //   </Layout>
+  // )
 }
 
 export default BlogPage
